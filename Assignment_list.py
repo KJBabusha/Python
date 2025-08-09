fd6446ed987c4aeb386bbe01e7ad8093e888dea2
KJBabusha <48787290+KJBabusha@users.noreply.github.com>
2025-08-09 12:11:33 +0300

    Add my week two assignment on List-operations.

---
+++ Assignment_list.py
@@ -0,0 +1,36 @@
+# make a empty list
+my_list = []
+ #add some items to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# print the current list
+print("Current List:", my_list)
+
+#find the index of a value in the list
+index_of_30 = my_list.index(30)
+# print the index of 30
+print("Index of 30:", index_of_30)
+
+# insert a value at the 2nd position
+my_list.insert(1, 15)
+print("List after inserting 15 at index 1:", my_list) 
+
+#Extend the list with another list
+my_list.extend([50, 60, 70])
+print("List after extending with [50, 60, 70]:", my_list)
+
+#remove the last element of the list
+my_list.pop(7)
+print("List after popping the last element:", my_list)
+
+#sort the list in ascending order
+my_list.sort(reverse=True)
+print("List after sorting in ascending order:", my_list)
+
+
+
+
+ 
